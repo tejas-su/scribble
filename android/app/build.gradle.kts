742e81d6ec1c714f1a8ec9642243f8eb92ae46e4
tejas-su <tejas_s.u@yahoo.com>
2025-07-06 12:26:32 +0530

    (fix) Android target sdk

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,14 +1,20 @@
+import java.util.Properties
+
 plugins {
     id("com.android.application")
     id("kotlin-android")
-    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
+}
+
+
+val keystoreProperties = Properties().apply {
+    load(File(rootDir, "keystore.properties").inputStream())
 }
 
 android {
     namespace = "com.widgetsandco.scribble"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
@@ -20,30 +26,41 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.example.test"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
 
-    release {
-        keyAlias keystoreProperties['keyAlias']
-        keyPassword keystoreProperties['keyPassword']
-        storeFile keystoreProperties['storeFile'] ? file(keystoreProperties['storeFile']) : null
-        storePassword keystoreProperties['storePassword']
+    signingConfigs {
+        create("release") {
+            keyAlias = keystoreProperties["keyAlias"] as String
+            keyPassword = keystoreProperties["keyPassword"] as String
+            storeFile = file(keystoreProperties["storeFile"] as String)
+            storePassword = keystoreProperties["storePassword"] as String
         }
+    }
 
     buildTypes {
-         release {
-           signingConfig signingConfigs.release
-       }
+        getByName("release") {
+            isMinifyEnabled = true
+            isShrinkResources = true 
+            signingConfig = signingConfigs.getByName("release")
+        }
     }
 }
 
 flutter {
     source = "../.."
+}
+
+tasks.register("printAndroidConfig") {
+    doLast {
+        println("compileSdk: ${android.compileSdk}")
+        println("minSdk: ${android.defaultConfig.minSdk}")
+        println("targetSdk: ${android.defaultConfig.targetSdk}")
+        println("versionCode: ${android.defaultConfig.versionCode}")
+        println("versionName: ${android.defaultConfig.versionName}")
+    }
 }
