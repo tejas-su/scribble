2f08e29c74ba73f0c075e2f48a41c6337fb68190
tejas-su <tejas_s.u@yahoo.com>
2025-10-20 12:00:32 +0530

    (feat-share) Merged the migration branch to master

--- lib/src/features/notes/presentation/widgets/notes_card.dart
+++ lib/src/features/notes/presentation/widgets/notes_card.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_slidable/flutter_slidable.dart';
 
 class NotesCard extends StatelessWidget {
   /// On tap function: When the user taps on the card
